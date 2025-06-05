53143b930125eac0661dc8c444b176de112f3e23
Emily <lackersh@grinnell.edu>
2025-06-05 15:38:01 -0500

    committing to merge

--- sod/simpliciality/Emily_edge_rewiring.py
+++ sod/simpliciality/Emily_edge_rewiring.py
@@ -1,23 +1,40 @@
+#TO RUN: python -m sod.simpliciality.Emily_edge_rewiring  
+
 import numpy as np
 import xgi
 from ..trie import Trie
 from .utilities import missing_subfaces, powerset
+from itertools import chain, combinations
+
 
 def h1():
     return xgi.Hypergraph([{1, 2, 3}, {2, 3, 4, 5}, {5, 6, 7}, {5, 6}])
 
-def non_simplex_maximal_edges(H, min_size=2, exclude_min_size=True):
-    #filters all edges so that size is greater than or equal to min size 
-    edges = H.edges.filterby("size", min_size, "geq").members()
-    #excludes edges smaller or equal to than min size + 1
-    max_edges = (
-        H.edges.maximal().filterby("size", min_size + exclude_min_size, "geq").members()
-    )
-    #creates trie from edges
-    t = Trie()
-    t.build_trie(edges)
+def Alg_1(H, min_size=2, exclude_min_size=True):
+    #gets all edges from hypergraph
+    edges = H.edges.members()
+    print("made edges")
+    pedges = powerset(edges, min_size, None)
+    print("made powerset")
+    #sorted_edges = sorted(edges, key=len, reverse=True)           don't need
+    edge_index = 0
+    print("getting to while loop")
+    print("edge:", edges[edge_index])
+    #for e in pedges:
+    #    print(list(e))
 
-    print(t)
+    while(len(edges[edge_index]) < 1):
+        print("edge_index:", edge_index)
+        edge_index += 1
+        print("edge_index:", edge_index)
+    e = edges[edge_index]
+    pe = set(powerset(e, min_size, None))
+
+    if((pe - e) != 0):
+        e1 = edge(random.randint(0, len(edge)), )
+        print(pe)
+        print("entered")
+    print("returning")
     return H
 
 dataset = "email-enron"
@@ -25,4 +42,4 @@ max_order = 5
 min_size = 2
 H = xgi.load_xgi_data(dataset, max_order=max_order)
 H.cleanup(singletons=True)
-non_simplex_maximal_edges(H, 1, True)
+Alg_1(H, 1, True)
