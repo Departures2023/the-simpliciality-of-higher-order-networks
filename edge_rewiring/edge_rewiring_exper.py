d2e85650fa60013722fd6649c0f4c91e357f6e88
liyutong721002 <liyutong721002@gmail.com>
2025-06-09 14:07:13 -0500

    some updates

--- edge_rewiring/edge_rewiring_exper.py
+++ edge_rewiring/edge_rewiring_exper.py
@@ -11,7 +11,7 @@ from sod import *
 from sod.simpliciality import edit_simpliciality
 import threading
 from edge_rewiring import edge_rewiring_alg
-from colorama import init
+from colorama import Fore
 from termcolor import colored
 
 datasets = [
@@ -42,7 +42,9 @@ max_order = 11
 min_size = 2
 
 
-def edge_rewiring_exper(int):
+def edge_rewiring_exper(int, times):
+    total_time = 0
+    delta_ES = 0
     for j in range (3):
         H = xgi.load_xgi_data(datasets[int], max_order=max_order)
         H.cleanup(singletons=True)
@@ -50,13 +52,23 @@ def edge_rewiring_exper(int):
         H, stats = edge_rewiring_alg.rewire_Alg1_expr(H, min_size, max_order)
         H.cleanup(singletons=True)
         edge_rewiring_alg.save_expr_data(datasets[int], j, stats, dir[datasets[int]])
+        total_time += stats["total_time"]
+        delta_SF += stats["delta_SF"]
+        delta_ES += stats["delta_ES"]
+        delta_FES += stats["delta_FES"]
         print(colored(datasets[int], 'blue'), stats)  
+        
+    print( Fore.LIGHTGREEN_EX + str(datasets[int]) + ": total_time = " + str(total_time) + 
+          " delta_SF = " + str(delta_SF) + 
+          " delta_ES = " + str(delta_ES) + 
+          " delta_FES = " + str(delta_FES))
+    
         
         
 if __name__ =="__main__": 
     threads = []
     for i in range(10):
-        thread = threading.Thread(target=edge_rewiring_exper, args=(i,))
+        thread = threading.Thread(target=edge_rewiring_exper, args=(i, 3, ))
         threads.append(thread)
         thread.start()
         
