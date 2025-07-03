8d96452158fea15767a9f51088b064e8d72d1123
Emily <lackersh@grinnell.edu>
2025-07-03 15:16:18 -0500

    update

--- edge_rewiring/edge_rewiring_exper.py
+++ edge_rewiring/edge_rewiring_exper.py
@@ -326,7 +326,7 @@ if __name__ == "__main__":
             )
             future.result()
         # frees memory hopefully
-        del graphs[i]
+        graphs[i] = None
         gc.collect()
 
     # Prints the results of the experiments
