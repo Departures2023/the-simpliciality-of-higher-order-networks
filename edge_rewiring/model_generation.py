f8e83b43ae53f8fde045783572c0f79ff6617283
Hachi <tangkevi@grinnell.edu>
2025-07-14 15:58:59 -0500

    temp change

--- edge_rewiring/model_generation.py
+++ edge_rewiring/model_generation.py
@@ -250,13 +250,13 @@ def model_generation_es(es, approx_num_C, num_max_hyperedge, num_node, min_size=
     # Calculate the average number of induced hyperedges
     C_avg = C_total / num_max_hyperedge
     if es < 0.15:
-        std = 0.5
-    elif es < 0.5:
-        std = 1
-    elif es < 0.85:
-        std = 2
-    else:
         std = 3
+    elif es < 0.5:
+        std = 2
+    elif es < 0.85:
+        std = 1
+    else:
+        std = 0.5
     # Print statements for debugging
     # print("edge_total:", edge_total)
     # print("C_total:", C_total)
